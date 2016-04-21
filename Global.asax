d00c91711e7b46f6c0fbefc4fa8777f040ef4289
kiranu <kiran.ug123@gmail.com>
2016-04-21 17:48:41 +0530

    initial commit 1

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_for_git_practice.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_for_git_practice.Startup))]
+namespace WebApplication_for_git_practice
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
